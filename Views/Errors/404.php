857ea2ce26bd0cb519d133bfec335aca23e02503
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-02-05 03:29:34 +0300

    Поправил страницу 404

--- Views/Errors/404.php
+++ Views/Errors/404.php
@@ -1,6 +1,6 @@
 <div class="page__block page--height-100 flex align-items--center">
     <div class="page__container">
-        <p class="page-title page-title--large" href="#">Страница не найдена</p>
+        <p class="page-title page-title--large" href="#">{ 404 } Страница не найдена</p>
         <p class="pt-5">Уважаемый пользователь!</p>
         <p>К сожалению, страница на которую вы перешли не существует.</p>
         <p>Попробуйте открыть страницу из главного меню или перейти на стартовую страницу, нажав на кнопку ниже.</p><a class="btn medium btn--info large mt-5" href="/" aria-label="На стартовую страницу">На стартовую страницу</a>
