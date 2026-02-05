87605ac7aea6e5c68a2463e569c1e3d5ae043349
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-02-05 04:59:19 +0300

    Добавил стилли списка проектов

--- Views/Admin/Content/Projects/list.php
+++ Views/Admin/Content/Projects/list.php
@@ -1,7 +1,105 @@
-<div class="page__block main-header theme--blue py-10">
-    <div class="page__container main-header__inner">
-
+<div class="flex justify-content--between align-items--center mb-5">
+    <a href="/admin/project/create" class="btn btn--info btn--blue">Создать проект</a>
+    <div class="search-box">
+        <input type="text" placeholder="Поиск по проектам..." class="form__input">
     </div>
 </div>
 
-<script src="/assets/scripts/projects-list.js" type="module"></script>
+<div class="projects-crud-list">
+    <div class="crud-project__wrapper"><div class="crud-project">
+        <header>
+            <p class="crud-project__title">[1] Unity Developer Toolkit</p>
+
+            <div class="crud-project__control">
+                <a href="#" class="btn square btn--info icon icon-edit"></a>
+                <a href="#" class="btn square btn--danger icon icon-delete"></a>
+            </div>
+        </header>
+
+        <div class="crud-project__main">
+            <img class="crud-project__icon" src="/assets/images/project_icon.png" alt="Unity Developer Toolkit">
+            <div>
+                <span class="text--weight-4">Unity Developer Toolkit (UDT)</span> - это набор инструментов для Unity разработчика который поможет
+                ускорить процесс разработки через набор готовых скриптов, префабов и спрайтов. Теперь можно в пару нажатий
+                обрабатывать столкновения, создавать счетчик, кошелек персонажа и другое.
+            </div>
+        </div>
+
+        <footer>
+            <div class="tags">
+                <span class="tag">C#</span>
+                <span class="tag">Unity</span>
+                <span class="tag">Demo</span>
+            </div>
+
+            <div class="crud-project__links">
+                <button class="btn btn--secondary">Git</button>
+                <button class="btn btn--info">Demo</button>
+            </div>
+        </footer>
+    </div></div>
+    <div class="crud-project__wrapper"><div class="crud-project">
+        <header>
+            <p class="crud-project__title">[2] Unity Developer Toolkit</p>
+
+            <div class="crud-project__control">
+                <a href="#" class="btn square btn--info icon icon-edit"></a>
+                <a href="#" class="btn square btn--danger icon icon-delete"></a>
+            </div>
+        </header>
+
+        <div class="crud-project__main">
+            <img class="crud-project__icon" src="/assets/images/project_icon.png" alt="Unity Developer Toolkit">
+            <div>
+                <span class="text--weight-4">Unity Developer Toolkit (UDT)</span> - это набор инструментов для Unity разработчика который поможет
+                ускорить процесс разработки через набор готовых скриптов, префабов и спрайтов. Теперь можно в пару нажатий
+                обрабатывать столкновения, создавать счетчик, кошелек персонажа и другое.
+            </div>
+        </div>
+
+        <footer>
+            <div class="tags">
+                <span class="tag">C#</span>
+                <span class="tag">Unity</span>
+                <span class="tag">Demo</span>
+            </div>
+
+            <div class="crud-project__links">
+                <button class="btn btn--secondary">Git</button>
+                <button class="btn btn--info">Demo</button>
+            </div>
+        </footer>
+    </div></div>
+    <div class="crud-project__wrapper"><div class="crud-project">
+        <header>
+            <p class="crud-project__title">{ 3 } Unity Developer Toolkit</p>
+
+            <div class="crud-project__control">
+                <a href="#" class="btn square btn--info icon icon-edit"></a>
+                <a href="#" class="btn square btn--danger icon icon-delete"></a>
+            </div>
+        </header>
+
+        <div class="crud-project__main">
+            <img class="crud-project__icon" src="/assets/images/project_icon.png" alt="Unity Developer Toolkit">
+            <div>
+                <span class="text--weight-4">Unity Developer Toolkit (UDT)</span> - это набор инструментов для Unity разработчика который поможет
+                ускорить процесс разработки через набор готовых скриптов, префабов и спрайтов. Теперь можно в пару нажатий
+                обрабатывать столкновения, создавать счетчик, кошелек персонажа и другое.
+            </div>
+        </div>
+
+        <footer>
+            <div class="tags">
+                <span class="tag">C#</span>
+                <span class="tag">Unity</span>
+                <span class="tag">Demo</span>
+            </div>
+
+            <div class="crud-project__links">
+                <button class="btn btn--secondary">Git</button>
+                <button class="btn btn--info">Demo</button>
+            </div>
+        </footer>
+    </div></div>
+</div>
