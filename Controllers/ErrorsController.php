71e53f08f3ff7dde4e941a16f5ffc9c95cb65f17
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-02-16 18:47:43 +0300

    Реализовал страницу 403

--- Controllers/ErrorsController.php
+++ Controllers/ErrorsController.php
@@ -7,12 +7,21 @@ use Modules\Main\Template;
 
 class ErrorsController extends BaseController
 {
-    public function page404()
+    public function page404() : void
     {
         Template::getInstance()->setParam('title', 'Страница 404');
 
         Template::getInstance()->includeHeader();
         $this->render('404');
+        Template::getInstance()->includeFooter();
+    }
+
+    public function page403() : void
+    {
+        Template::getInstance()->setParam('title', 'Ошибка прав доступа');
+
+        Template::getInstance()->includeHeader();
+        $this->render('403');
         Template::getInstance()->includeFooter();
     }
 }
---
+++ Views/Errors/403.php
@@ -0,0 +1,9 @@
+<div class="page__block page--height-100 flex align-items--center">
+    <div class="page__container">
+        <p class="page-title page-title--large" href="#">{ 403 } Ошибка прав доступа</p>
+        <p class="pt-5">Уважаемый пользователь!</p>
+        <p>У вас недостаточно прав для просмотра этой страницы.</p>
+        <p>Попробуйте открыть страницу из главного меню или перейти на стартовую страницу, нажав на кнопку ниже.</p>
+        <a class="btn medium btn--info large mt-5" href="/" aria-label="На стартовую страницу">На стартовую страницу</a>
+    </div>
+</div>
