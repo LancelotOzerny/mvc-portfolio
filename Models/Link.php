ae1191e7a815be0cbb2d8ad3c9136c9291af3563
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-02-27 22:38:26 +0300

    Добавил сущности тега и ссылки

---
+++ Models/Link.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Models;
+
+use Modules\Orm\Entity;
+
+class Link extends Entity
+{
+    protected string $table = 'links';
+}
---
+++ Models/Tag.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Models;
+
+use Modules\Orm\Entity;
+
+class Tag extends Entity
+{
+    protected string $table = 'tags';
+}
